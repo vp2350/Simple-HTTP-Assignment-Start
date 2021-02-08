04c36413c7686dc34974507bb42c65dad868d6da
Vansh Pahuja <vp2350@rit.edu>
2021-02-07 23:25:38 -0500

    finished exercise

---
+++ src/imageResponses.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+const getImage = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(index);
+  response.end();
+};
+
+module.exports.getImage = getImage;
